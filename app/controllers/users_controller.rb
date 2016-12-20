1e3e339228f7a3438bc4c2254f6e4d6ffddc8d4f
Aaron Brown <aaroncbrown1993@gmail.com>
2016-12-20 11:38:16 -0500

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_user, only: [:show]
+
+	def index
+		case params[:people]
+			when "friends"
+			@users = current_user.active_friends
+			when "requests"
+			@users = current_user.pending_friend_requests_from
+			when "pending"
+			@users = current_user.pending_friend_requests_to			#created in the user model
+			else
+			@users = User.where.not(id: current_user.id)
+		end
+	end
+
+	def show
+		@post = Post.new
+		@posts = @user.posts.order('created_at DESC')
+		@activities = PublicActivity::Activity.where(owner_id: @user.id)
+	end
+
+	def set_user
+		@user = User.find_by(username: params[:id])
+	end
+end
